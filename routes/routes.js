1a8d80c65e53cd23c0087177a46fecf2e27e0faf
braulio0790 <braulio90.lds@gmail.com>
2023-05-23 00:16:25 -0600

    C1

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,6 @@ const swaggerDocument = require('../swagger.json');
 
 router.use('/', require('./swagger'));
 router.get('/towns', towns.getTowns);
-router.post('/towns', towns.updateTown);
+router.post('/towns/update/:town_id', towns.updateTown);
 
 module.exports = router;
